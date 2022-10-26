5b14dc2b6be12ba817c6f7f783e365ee4c59414b
sanaas246 <112659421+sanaas246@users.noreply.github.com>
2022-10-26 14:45:25 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,60 @@
+# Create Arrays Practice
+
+import random 
+
+# 1
+months = ["January", "June", "July"]
+
+# 2
+joyli = []
+for joy in range(700):
+    joyli.append("joy")
+
+# 3
+sevenli = []
+for sevens in range(500):
+    sevenli.append("7")
+
+# 4
+randomNums = []
+for rand in range(5000):
+    randomNums.append(random.randint(0,100))
+
+# 5
+randomInts = []
+for rand in range(300):
+    randomInts.append(random.randint(0,40))
+
+# # 6
+# multFour = []
+# factor = 1
+# loop = True
+# while loop:
+#     if factor * 4 > 20 and factor * 4 < 800:
+#         multFour.append(factor * 4)
+#         factor += 1
+#     else: 
+#         factor += 1
+# print(multFour)
+
+# 7 
+# evenli = []
+# factorE = 1
+# loop = True
+# while loop:
+#     if factorE * 2 > 10 and factorE * 2 < 100:
+#         evenli.insert(len(evenli), factorE * 2)
+#         factorE += 1
+#     else:
+#         factorE += 1
+# print(evenli)
+
+# # 8
+# colors_str = "red,orange,yellow,green,blue,indigo,violet"
+# for color in colors_str:
+#     print(f"{color}")
+
+# # 9
+# cities_str = "Edmonton;Calgary;Vancouver;Saskatoon;Winnipeg"
+# for city in cities_str:
+#     print(f"{city}")
